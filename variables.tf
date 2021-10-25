19aa33afa232ebb7437fac4adc4bd07fc103167d
Aleksey Mamaev <a.mamaevs@gmail.com>
2021-10-25 23:17:35 +0300

    11

---
+++ variables.tf
@@ -0,0 +1,29 @@
+
+variable "zone" {
+  default = ""
+}
+
+variable "token" {
+  default = ""
+}
+
+variable "cloud_id" {
+  default = ""
+}
+
+variable "folder_id" {
+  default = ""
+}
+
+variable "user" {
+  default = "ubuntu"
+
+}
+
+variable "sshrsa" {
+  default = "~/.ssh/id_rsa.pub"
+
+}
+variable "credentials" {
+  default = ""
+}
