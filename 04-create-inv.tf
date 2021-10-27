84a56bad9c0a19e7e311f4494ba83f5c40bd4d65
Aleksey Mamaev <a.mamaevs@gmail.com>
2021-10-27 17:07:19 +0300

    update

--- 04-create-inv.tf
+++ 04-create-inv.tf
@@ -1,8 +1,8 @@
 data "template_file" "k8s" {
   template = file("${path.module}/templates/inventory.tpl")
   vars = {
-    k8s_master   = join("\n", formatlist("%s ansible_ssh_host=%s ansible_ssh_user=${var.ssh_user}", yandex_compute_instance.k8s-master.*.name, yandex_compute_instance.k8s-master.*.network_interface.0.nat_ip_address))
-    k8s_worker   = join("\n", formatlist("%s ansible_ssh_host=%s ansible_ssh_user=${var.ssh_user}", yandex_compute_instance.k8s-worker.*.name, yandex_compute_instance.k8s-worker.*.network_interface.0.nat_ip_address))
+    k8s_master = join("\n", formatlist("%s ansible_ssh_host=%s ansible_ssh_user=${var.ssh_user}", yandex_compute_instance.k8s-master.*.name, yandex_compute_instance.k8s-master.*.network_interface.0.nat_ip_address))
+    k8s_worker = join("\n", formatlist("%s ansible_ssh_host=%s ansible_ssh_user=${var.ssh_user}", yandex_compute_instance.k8s-worker.*.name, yandex_compute_instance.k8s-worker.*.network_interface.0.nat_ip_address))
 
   }
 
